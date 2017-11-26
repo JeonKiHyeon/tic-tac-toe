28fe90d47c65640f7fee58a4c643310efba941b4
JeonKiHyeon <31892070+JeonKiHyeon@users.noreply.github.com>
2017-11-26 15:36:49 +0900

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,11 +3,16 @@ from tkinter import *
 def checked(i) :
       global player
       button = list[i]
+      win = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
 
       if button["text"] != "     " :
             return
       button["text"] = player 
       button["bg"] = "yellow"
+      
+      for x in win:
+            if player == list[x[0]]["text"] == list[x[1]]["text"] == list[x[2]]["text"]:
+                  tkinter.messagebox.showinfo("game over", "{0} player win!".format(player))
 
       if player == "X" :
             player = "O"
